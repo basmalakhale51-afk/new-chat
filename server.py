724a1ed3aa8bc6e485e5244feeb5ef328daa11bb
basmalakhale51-afk <basmalakhale51@gmail.com>
2026-04-19 05:38:41 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+clients = []
+
+def broadcast(msg, sender):
+    for client in clients:
+        if client != sender:
+            try:
+                client.send(msg)
+            except:
+                clients.remove(client)
+
+def handle_client(client):
+    while True:
+        try:
+            msg = client.recv(1024)
+            broadcast(msg, client)
+        except:
+            clients.remove(client)
+            client.close()
+            break
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = int(os.environ.get("PORT", 5555))
+server.bind(("0.0.0.0", PORT))
+server.listen()
+
+print("Server is running...")
+
+while True:
+    client, addr = server.accept()
+    print("Client connected:", addr)
+    clients.append(client)
+
+    thread = threading.Thread(target=handle_client, args=(client,))
+    thread.start()
